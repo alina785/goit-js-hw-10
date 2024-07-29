310c6fc7c5379a11a24ab227656b183858c4b14a
alina785 <142399750+alina785@users.noreply.github.com>
2024-07-29 11:28:47 +0000

    Deploying to gh-pages from @ alina785/goit-js-hw-10@477387d4a33edad69e513960d8fb74d6921518fa 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.delay.value),o=t.target.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
